069c7261dc0204b0805af46ae5330efb6abe8738
Aaron M <webgovernor@gmail.com>
2018-01-27 09:54:58 -0700

    Add totalWealth 2

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -11,7 +11,7 @@
         </v-card-text>
         <v-subheader>Total Wealth</v-subheader>
         <v-card-text>
-          <v-slider v-model="totalWealth" thumb-label step="100" min="200" max="10000" ticks></v-slider>
+          <v-slider v-model="totalWealth" thumb-label step="100" min="1000" max="10000" ticks></v-slider>
           The total amount of wealth within the economy. Since it's a closed economy, wealth can never be more than this amount.
         </v-card-text>
       </v-card>
