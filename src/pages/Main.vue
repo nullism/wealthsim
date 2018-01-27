4f492f87c71950c59a75d9194a1a06b2a38c931d
Aaron M <webgovernor@gmail.com>
2018-01-27 09:42:56 -0700

    Add v-card formatting

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,7 +1,13 @@
 <template>
   <v-app id="wealthsim">
     <h2>Wealth Sim</h2>
-    <v-slider v-model="actorCount" thumb-label step="10" min="50" max="200" ticks></v-slider>
+    <v-card>
+      <v-subheader>Number of People</v-subheader>
+      <v-card-text>
+        <v-slider v-model="actorCount" thumb-label step="10" min="50" max="200" ticks></v-slider>
+        This determines the number of people participating in the simulated economy. More people takes longer to simulate.
+      </v-card-text>
+    </v-card>
   </v-app>
 </template>
 
