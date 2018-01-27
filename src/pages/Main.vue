85149693082bbf39c0b764fade7081d5e253ddff
Aaron M <webgovernor@gmail.com>
2018-01-27 09:49:44 -0700

    Add v-card formatting 2

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app id="wealthsim">
     <v-expansion-panel expand>
-      <div slot="header">Basic Settings</div>
-      <v-card>
-        <v-subheader>Number of People</v-subheader>
-        <v-card-text>
-          <v-slider v-model="actorCount" thumb-label step="10" min="50" max="200" ticks></v-slider>
-          This determines the number of people participating in the simulated economy. More people takes longer to simulate.
-        </v-card-text>
-      </v-card>
+      <v-expansion-panel-content>
+        <div slot="header">Basic Settings</div>
+        <v-card>
+          <v-subheader>Number of People</v-subheader>
+          <v-card-text>
+            <v-slider v-model="actorCount" thumb-label step="10" min="50" max="200" ticks></v-slider>
+            This determines the number of people participating in the simulated economy. More people takes longer to simulate.
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
     </v-expansion-panel>
   </v-app>
 </template>
