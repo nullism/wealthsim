2bc83d4d318fd9481cb2db22e5ff3c2e8eefe252
Aaron M <webgovernor@gmail.com>
2018-01-26 23:17:06 -0700

    Add stepper 1

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app id="wealthsim">
     <h2>Wealth Sim</h2>
+    <v-slider v-model="actorCount" thumb-label step="10" min="50" max="200" ticks></v-slider>
   </v-app>
 </template>
 
 <script>
 
   export default {
+    data() {
+      return {
+        actorCount: 100,
+        totalWealth: 1000,
+      }
+
+    },
     components: {
     }
   }
