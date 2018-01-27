7b85695262f5d876858dd33a0853e0b199d1d998
Aaron M <webgovernor@gmail.com>
2018-01-27 10:46:48 -0700

    Add to sim 9

--- src/simulation.js
+++ src/simulation.js
@@ -38,61 +38,79 @@ export default class Simulation {
 
   doCycle(n) {
     for (let actor of this.actors) {
-      this.sharePrice = (
-        (this.boughtShares / this.totalShares) *
-        (this.conf.totalWealth / this.conf.actorCount)
-      )
-      let sharesLeft = this.totalShares - this.boughtShares
-      let investable = actor.wealth * this.conf.investPct
 
-      // Buy Share
-      if (sharesLeft > 0 && investable > this.sharePrice) {
-        actor.wealth -= this.sharePrice;
-        actor.shares += 1;
-        this.investmentPool += this.sharePrice;
-        this.boughtShares += 1;
-      }
-
-      // Pay dividends
-      if (this.boughtShares > 0) {
-        let sharePct = actor.shares / boughtShares;
-        if (sharePct > 0) {
-          let earned = this.investmentPool * sharePct;
-          actor.wealth += earned;
-          this.investmentPool -= earned;
-        }
-      }
+      // Investment
+      this.doInvestment(actor)
 
       // Spending
-      if (actor.wealth >= this.conf.spendAmount) {
-        actor.wealth -= this.conf.spendAmount;
-        actor.spent += this.conf.spendAmount;
+      this.doSpend(actor)
 
-        if (Math.random() < this.conf.companyChance && this.conf.investPct > 0) {
-          // Buy something from company
-          this.investmentPool += this.conf.spendAmount;
-        } else {
+      // Wealth Redistribution
+      this.doRedistribution(actor)
 
-          // Give to another random actor
-          let actor2 = this.actors[Math.floor(Math.random() * this.actors.length)];
-          actor2.wealth += this.conf.spendAmount;
-        }
+    }
+  }
+
+  doInvestment(actor) {
+    if (this.conf.investPct === 0) return null
+
+    this.sharePrice = (
+      (this.boughtShares / this.totalShares) *
+      (this.conf.totalWealth / this.conf.actorCount)
+    )
+    let sharesLeft = this.totalShares - this.boughtShares
+    let investable = actor.wealth * this.conf.investPct
+
+    // Buy Share
+    if (sharesLeft > 0 && investable > this.sharePrice) {
+      actor.wealth -= this.sharePrice;
+      actor.shares += 1;
+      this.investmentPool += this.sharePrice;
+      this.boughtShares += 1;
+    }
+
+    // Pay dividends
+    if (this.boughtShares > 0) {
+      let sharePct = actor.shares / boughtShares;
+      if (sharePct > 0) {
+        let earned = this.investmentPool * sharePct;
+        actor.wealth += earned;
+        this.investmentPool -= earned;
       }
+    }
+  }
 
-      // Wealth redistribution
-      let distribute = actor.wealth * this.conf.redistribution;
-      if (distribute > 0) {
-        this.redistributionPool += distribute;
-        actor.wealth -= distribute;
-      }
+  doSpend(actor) {
 
-      // Collect redistribution
-      if (this.redistributionPool > 0) {
-        let collect = this.redistributionPool / this.conf.actorCount;
-        this.redistributionPool -= collect;
-        actor.wealth += collect;
-      }
+    if (actor.wealth < this.conf.spendAmount) return null
 
+    actor.wealth -= this.conf.spendAmount;
+    actor.spent += this.conf.spendAmount;
+
+    if (Math.random() < this.conf.companyChance && this.conf.investPct > 0) {
+      // Buy something from company
+      this.investmentPool += this.conf.spendAmount;
+    } else {
+
+      // Give to another random actor
+      let actor2 = this.actors[Math.floor(Math.random() * this.actors.length)];
+      actor2.wealth += this.conf.spendAmount;
+    }
+  }
+
+  doRedistribution(actor) {
+    // Wealth redistribution
+    let distribute = actor.wealth * this.conf.redistribution;
+    if (distribute > 0) {
+      this.redistributionPool += distribute;
+      actor.wealth -= distribute;
+    }
+
+    // Collect redistribution
+    if (this.redistributionPool > 0) {
+      let collect = this.redistributionPool / this.conf.actorCount;
+      this.redistributionPool -= collect;
+      actor.wealth += collect;
     }
   }
 
