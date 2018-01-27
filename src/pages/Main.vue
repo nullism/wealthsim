57d955e7a2d8de400ddb724124f1d1a01e0d5413
Aaron M <webgovernor@gmail.com>
2018-01-27 10:04:48 -0700

    Add to sim

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,23 +1,29 @@
 <template>
-
-  <v-expansion-panel expand>
-    <v-expansion-panel-content>
-      <div slot="header">Basic Settings</div>
-      <v-card>
-        <v-subheader>Number of People</v-subheader>
-        <v-card-text>
-          <v-slider v-model="sim.actorCount" thumb-label step="10" min="50" max="200" ticks></v-slider>
-          This determines the number of people participating in the simulated economy. More people takes longer to simulate.
-        </v-card-text>
-        <v-subheader>Total Wealth</v-subheader>
-        <v-card-text>
-          <v-slider v-model="sim.totalWealth" thumb-label step="1000" min="1000" max="10000" ticks></v-slider>
-          The total amount of wealth within the economy. Since it's a closed economy, wealth can never be more than this amount.
-        </v-card-text>
-      </v-card>
-    </v-expansion-panel-content>
-  </v-expansion-panel>
-
+  <div>
+    <v-card>
+      <v-card-text>
+        Poorest Person: ${{res.poorestActor}}
+        Richest Person: ${{res.richestActor}}
+      </v-card-text>
+    </v-card>
+    <v-expansion-panel expand>
+      <v-expansion-panel-content>
+        <div slot="header">Basic Settings</div>
+        <v-card>
+          <v-subheader>Number of People</v-subheader>
+          <v-card-text>
+            <v-slider v-model="sim.actorCount" thumb-label step="10" min="50" max="200" ticks></v-slider>
+            This determines the number of people participating in the simulated economy. More people takes longer to simulate.
+          </v-card-text>
+          <v-subheader>Total Wealth</v-subheader>
+          <v-card-text>
+            <v-slider v-model="sim.totalWealth" thumb-label step="1000" min="1000" max="10000" ticks></v-slider>
+            The total amount of wealth within the economy. Since it's a closed economy, wealth can never be more than this amount.
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </div>
 </template>
 
 <script>
@@ -29,10 +35,13 @@
           actorCount: 100,
           totalWealth: 10000,
         },
-        actorCount: 100,
-        totalWealth: 1000,
       }
 
+    },
+    computed: {
+      res() {
+        return {}
+      }
     },
     components: {
     }
