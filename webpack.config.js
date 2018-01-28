44ed08874dba534763d45d2f642e0a8176c155a3
Aaron M <webgovernor@gmail.com>
2018-01-28 09:26:05 -0700

    Add css import 9

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = [
         },
         {
           test: /\.css$/,
-          loader: "style!css-loader",
+          loader: "style-loader!css-loader",
         }
       ]
     },
