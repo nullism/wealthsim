f3dddf428cf23685cb749f0c5dde475eb4ea7a76
Aaron M <webgovernor@gmail.com>
2018-01-29 10:52:26 -0700

    Theming 11

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -8,7 +8,7 @@
         <span class="grey--text text--lighten-2">Cycles Ran: {{ cyclesDone }} / {{ conf.cycleCount }}</span>
       </v-card-text>
     </v-card>
-    <v-card flat light>
+    <v-card flat style="background: transparent;">
       <v-card-text>
 
 
